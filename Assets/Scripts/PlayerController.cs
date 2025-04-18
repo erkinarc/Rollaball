a1baafd21a3658944e309f771abf0f533e4baff4
Erkin Arıcı <goktanarici@gmail.com>
2025-04-18 19:36:53 +0300

    Added UI Elements and Enemy
    Added counter text and win text
    Added Enemy Prefab and material

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,18 +3,25 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
+    private int count;
     private float movementX;
     private float movementY;
     public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winTextObject.SetActive(false);
     }
 
     void OnMove(InputValue movementValue)
@@ -24,13 +31,35 @@ public class PlayerController : MonoBehaviour
         movementX = movementVector.x;
         movementY = movementVector.y;
     }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 7)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
+
     private void FixedUpdate()
     {
         // Physics-related code can be placed here
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-        
+
         rb.AddForce(movement * speed);
-        
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if(other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+
     }
 
 }
