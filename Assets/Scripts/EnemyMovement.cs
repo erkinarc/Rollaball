2090167333b5b6bd8651b08ee4083a7c76024677
Erkin Arıcı <goktanarici@gmail.com>
2025-04-18 19:56:09 +0300

    enemy follow ai and static obstacles

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -1,16 +1,25 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour
 {
+
+    public Transform player; // The target to follow    
+    private NavMeshAgent navMeshAgent; // The NavMeshAgent component
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        navMeshAgent = GetComponent<NavMeshAgent>(); // Get the NavMeshAgent component attached to this GameObject
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (player != null)
+        {
+            // Set the destination of the NavMeshAgent to the player's position
+            navMeshAgent.SetDestination(player.position);
+        }
     }
 }
